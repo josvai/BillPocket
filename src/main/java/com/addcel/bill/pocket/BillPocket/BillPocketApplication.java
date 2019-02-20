db16cda4cfe076460a8e51a3f408ceaedc823c7f
Josue <josue.ocampo@addcel.com>
2019-02-20 12:41:03 -0600

    Se agrega librerias servlet-api

--- src/main/java/com/addcel/bill/pocket/BillPocket/BillPocketApplication.java
+++ src/main/java/com/addcel/bill/pocket/BillPocket/BillPocketApplication.java
@@ -8,7 +8,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
